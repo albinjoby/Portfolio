49e555b93aa9bca6971b0f9c253e06ce30b6b2a5
Albin <albinjobyponnu@gmail.com>
2025-02-24 22:07:01 +0530

    added rings and media queries

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,24 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
-import ArrowDown from "@/assets/icons/arrow-down.svg"
 import Image from "next/image";
+import ArrowDown from "@/assets/icons/arrow-down.svg"
+import grainImage from "@/assets/images/grain.jpg"
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{ backgroundImage: `url(${grainImage.src})` }}
+      ></div>
+      <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
             src={memojiImage}
-            className="size-[100px]"
+            className="size-[100px] md:size-[110px] lg:size-[130px]"
             alt="person peaking from behind the laptop"
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
@@ -19,14 +28,16 @@ export const HeroSection = () => {
             </div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
-          Building Exceptional User Experiences
-        </h1>
-        <p className="mt-4 text-center text-white/60">
-          I specialize in transforming designs into functional, high- performing
-          web applications. Let's discuss your next project.
-        </p>
-        <div className="flex flex-col items-center mt-8 gap-4">
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl text-center md:text-5xl mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional, high-
+            performing web applications. Let's discuss your next project.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
